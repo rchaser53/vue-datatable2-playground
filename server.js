66c7c1ed0007af1fc64f83ffaeb8033f8712f9fd
rchaser53 <tayoshizawa29@gmail.com>
2017-11-12 15:04:52 +0900

    try to confirm the request error
    maybe the error is nothing.
    need to search how to use 'hyper'

---
+++ server.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch')
+const express = require('express')
+
+const app = express()
+const server = require('http').createServer(app)
+
+app.get('*', async (req, res) => {
+  try {
+    const pre = await fetch('https://api.github.com/repos/rchaser53/vue-datatable2-playground/commits')
+    const ret = await pre.json()
+    res.send(JSON.stringify(ret))
+  }
+  catch (err) {
+    console.error(err)
+  }
+});
+
+server.listen(3000, () => {
+  console.log('run server');
+});
