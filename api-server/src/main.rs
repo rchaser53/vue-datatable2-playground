73f82f7ccad7b760c98b05e0d1d46f4023707797
rchaser53 <tayoshizawa29@gmail.com>
2017-11-12 14:51:37 +0900

    send https request but git response is Forbidden
    change core immutable => muttable

--- api-server/src/main.rs
+++ api-server/src/main.rs
@@ -1,14 +1,18 @@
 extern crate hyper;
+extern crate hyper_tls;
 extern crate futures;
 extern crate tokio_core;
 
+use std::io::{self, Write};
+use futures::future::{Future};
+use futures::stream::{Stream};
 use hyper::Client;
+use tokio_core::reactor::Core;
+
 use hyper::header::ContentLength;
 use hyper::server::{Http, Request, Response, Service};
+use hyper_tls::HttpsConnector;
 
-use futures::future::Future;
-
-use tokio_core::reactor::Core;
 
 const PHRASE: &'static str = "Hello, World!";
 
@@ -20,9 +24,6 @@ impl Service for HelloWorld {
     type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
 
     fn call(&self, _req: Request) -> Self::Future {
-        // We're currently ignoring the Request
-        // And returning an 'ok' Future, which means it's ready
-        // immediately, and build a Response with the 'PHRASE' body.
         let mut core = Core::new().unwrap();
         let client = Client::new(&core.handle());
 
@@ -36,26 +37,29 @@ impl Service for HelloWorld {
 
 fn main() {
     let mut core = Core::new().unwrap();
-    let client = Client::new(&core.handle());
+    let handle = core.handle();
+    let client = Client::configure()
+        .connector(HttpsConnector::new(4, &handle).unwrap())
+        .build(&handle);
 
-    // let uri = "https://api.github.com/repos/rchaser53/vue-datatable2-playground/commits"
-    let uri = "http://httpbin.org/ip"
+    let uri = "https://api.github.com/repos/rchaser53/vue-datatable2-playground/commits"
                     .parse().unwrap();
+    let work = client.get(uri);
 
-    let work = client.get(uri)
-                .map(|res| {
-                // println!("{}", res);
-                println!("Response: {:?}", res.status());
-                println!(11);
-            });
-
-    core.run(work).unwrap();
+    let res = core.run(work).unwrap();
+    println!("{:?}", res.status().is_success());
 
     // let addr = "127.0.0.1:3000".parse().unwrap();
     // let server = Http::new().bind(&addr, || Ok(HelloWorld)).unwrap();
     // server.run().unwrap();
 }
 
+
+                // res.body().for_each(|chunk| {
+                //     io::stdout()
+                //         .write_all(&chunk)
+                //         .map_err(From::from)
+                // })
 
                 // Response::new()
                     // .with_header(ContentLength(PHRASE.len() as u64))
