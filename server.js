1592151e574d84f1d694b250aba9dbfd67a0fabf
rchaser53 <tayoshizawa29@gmail.com>
2017-11-20 00:07:22 +0900

    fetch the tabledata from api server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const path = require('path')
 const fetch = require('node-fetch')
 const express = require('express')
 
+const fakeData = require('./fakeData')
 const app = express()
 const server = require('http').createServer(app)
 
-app.get('*', async (req, res) => {
+app.get('/', async (req, res) => {
   try {
     const pre = await fetch('https://api.github.com/repos/rchaser53/vue-table-playground/commits')
     const ret = await pre.json()
@@ -15,6 +17,20 @@ app.get('*', async (req, res) => {
   }
 });
 
-server.listen(3000, () => {
+app.get('/tabledata', async (req, res) => {
+  try {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.send(JSON.stringify(fakeData))
+  }
+  catch (err) {
+    console.error(err)
+  }
+});
+
+app.get('/index', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+server.listen(3100, () => {
   console.log('run server');
 });
