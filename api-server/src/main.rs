4a1748db46dd055f5e54028deca4abff3a0bf5c8
rchaser53 <tayoshizawa29@gmail.com>
2017-11-19 16:52:41 +0900

    try to use routing but failed to  use glob
    cannot use glob for routing?

--- api-server/src/main.rs
+++ api-server/src/main.rs
@@ -5,7 +5,7 @@ extern crate tokio_core;
 extern crate serde_json;
 
 use futures::future::{Future};
-use hyper::{Client, Method};
+use hyper::{Client, Method, StatusCode};
 use tokio_core::reactor::Core;
 
 use hyper::header::{Headers, UserAgent, ContentLength};
@@ -19,27 +19,30 @@ impl Service for HelloWorld {
     type Error = hyper::Error;
     type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
 
-    fn call(&self, _req: Request) -> Self::Future {
+    fn call(&self, req: Request) -> Self::Future {
+        let mut response = Response::new();
         let mut core = Core::new().unwrap();
         let handle = core.handle();
-
-        let mut req = Request::new(Method::Get, "https://api.github.com/repos/rchaser53/vue-table-playground/commits".parse().unwrap());
-        req.headers_mut().set(UserAgent::new("todo"));
-
         let client = Client::configure()
             .connector(HttpsConnector::new(4, &handle).unwrap())
             .build(&handle);
 
-        let work = client.request(req);
-        let res = core.run(work).unwrap();
-
-        println!("run server");
-
-        Box::new(futures::future::ok(
-            Response::new()
-                // .with_header(ContentLength(res.body().len() as u64))
-                .with_body(res.body())
-        ))
+        match (req.method(), req.path()) {
+            (&Method::Get, "/*") => {
+                response.set_body("nya-n");
+            },
+            (&Method::Get, "/git") => {
+                let mut req = Request::new(Method::Get, "https://api.github.com/repos/rchaser53/vue-table-playground/commits".parse().unwrap());
+                req.headers_mut().set(UserAgent::new("todo"));
+                let work = client.request(req);
+                let _res = core.run(work).unwrap();
+                response.set_body(_res.body());
+            },
+            _ => {
+                response.set_status(StatusCode::NotFound);
+            },
+        };
+        Box::new(futures::future::ok(response))
     }
 }
 
@@ -48,6 +51,10 @@ fn main() {
     let server = Http::new().bind(&addr, || Ok(HelloWorld)).unwrap();
     server.run().unwrap();
 }
+
+            // Response::new()
+            //     // .with_header(ContentLength(res.body().len() as u64))
+            //     .with_body(res.body())
 
     // header! { ( Authorization, "Authorization" ) => [String] }
     // let auth_header = format!("token {}", token);
